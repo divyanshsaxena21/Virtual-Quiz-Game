ff6eefa848fff8a59e4b15ce2787763511ec4731
Divyansh Saxena <129607728+divyanshsaxena21@users.noreply.github.com>
2024-09-23 11:58:49 +0530

    Add files via upload

---
+++ Game/dbconnector.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+
+class DatabaseOperations:
+
+  def __init__(self):
+    myconn = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="kanpur",
+      # host="10.8.9.75",
+      # user="devesh",
+      # password="mishra@123",
+      database="project",
+      auth_plugin='mysql_native_password'
+    )
+    self.conn = myconn
+    self.cursor = myconn.cursor()
+  def getQuestionsFromDatabase(self, tableName):
+
+    query="select * from " + tableName
+    self.cursor.execute(query)
+    myresult = self.cursor.fetchall()
+    # for row in myresult:
+    #   print(row)
+    return myresult
+
+  def closeConnetion(self):
+    self.cursor.close()
+    self.conn.close()
+
+
+# databaseConnection = DatabaseOperations()
+# dataAll = databaseConnection.getQuestionsFromDatabase("demo")
